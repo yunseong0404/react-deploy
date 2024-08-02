f0da872fa02235c154b5f3afd99caac8ad744c6a
yunseong0404 <333665@naver.com>
2024-08-03 08:20:27 +0900

    remove: mock api 삭제

--- src/index.tsx
+++ src/index.tsx
@@ -7,19 +7,8 @@ import App from '@/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
-async function deferRender() {
-  if (process.env.REACT_APP_RUN_MSW === 'true') {
-    const { worker } = await import('./mocks/browser');
-    await worker.start();
-  }
-
-  return;
-}
-
-deferRender().then(() => {
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  );
-});
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
