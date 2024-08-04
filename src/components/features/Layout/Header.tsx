10d620c8e9d8ae95c6967fc2d417258509a475d1
yunseong0404 <333665@naver.com>
2024-08-05 00:31:39 +0900

    feat: select API 구현

--- src/components/features/Layout/Header.tsx
+++ src/components/features/Layout/Header.tsx
@@ -1,5 +1,6 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Select } from '@chakra-ui/react';
 import styled from '@emotion/styled';
+import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
 import { Container } from '@/components/common/layouts/Container';
@@ -9,9 +10,24 @@ import { getDynamicPath, RouterPath } from '@/routes/path';
 export const Header = () => {
   const navigate = useNavigate();
   const authInfo = useAuth();
+  const [selectedApi, setSelectedApi] = useState('');
+
+  const API: { [key: string]: string } = {
+    신예준: 'https://pnuece.pnu.app',
+    원윤서: 'http://3.37.113.212:8080',
+    이영준: '이영준',
+    황수환: 'https://kakao-gift-api.shop',
+  };
 
   const handleLogin = () => {
     navigate(getDynamicPath.login());
+  };
+
+  const handleApiChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const apiName = event.target.value;
+    setSelectedApi(apiName);
+    sessionStorage.setItem('BASE_URL', API[apiName]);
+    window.location.reload();
   };
 
   return (
@@ -24,8 +40,19 @@ export const Header = () => {
           />
         </Link>
         <RightWrapper>
+          <Select
+            width={180}
+            placeholder="백엔드 API 선택"
+            onChange={handleApiChange}
+            value={selectedApi}
+          >
+            <option value="신예준">신예준</option>
+            <option value="원윤서">원윤서</option>
+            <option value="이영준">이영준</option>
+            <option value="황수환">황수환</option>
+          </Select>
           {authInfo ? (
-            <Box display="flex" gap="15px" >
+            <Box display="flex" gap="15px">
               <LinkButton onClick={() => navigate(RouterPath.admin)}>관리자</LinkButton>
               <LinkButton onClick={() => navigate(RouterPath.myAccount)}>내 계정</LinkButton>
             </Box>
@@ -53,7 +80,12 @@ export const Wrapper = styled.header`
 const Logo = styled.img`
   height: ${HEADER_HEIGHT};
 `;
-const RightWrapper = styled.div``;
+const RightWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 15px;
+`;
 
 const LinkButton = styled.p`
   align-items: center;
