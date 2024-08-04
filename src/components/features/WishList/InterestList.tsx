5badb99a9cf92ff1ff48ca36c075c268d903c679
yunseong0404 <333665@naver.com>
2024-08-05 00:28:42 +0900

    feat: WishListPage 구현

---
+++ src/components/features/WishList/InterestList.tsx
@@ -0,0 +1,99 @@
+import { Button } from '@chakra-ui/button';
+import { Container } from '@chakra-ui/layout';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+import { BASE_URL } from '@/api/instance';
+import { DefaultGoodsItems } from '@/components/common/GoodsItem/Default';
+import { Grid } from '@/components/common/layouts/Grid';
+import { breakpoints } from '@/styles/variants';
+
+type WishProduct = {
+  id: number;
+  option: {
+    id: number;
+    name: string;
+    product: {
+      id: number;
+      name: string;
+      price: number;
+      imageUrl: string;
+    };
+  };
+};
+
+export default function InterestList() {
+  const [wishes, setWishes] = useState<WishProduct[]>([]);
+
+  useEffect(() => {
+    fetchWishes();
+  }, []);
+
+  const fetchWishes = async () => {
+    const token = localStorage.getItem('token');
+    try {
+      const response = await axios.get(`${BASE_URL}/api/wishes`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      setWishes(response.data);
+    } catch (err) {
+      console.log('관심 목록을 불러오는 데 실패했습니다.');
+    }
+  };
+
+  const handleDeleteWish = async (wishId: number) => {
+    const token = localStorage.getItem('token');
+    try {
+      await axios.delete(`${BASE_URL}/api/wishes/${wishId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setWishes(wishes.filter((wish) => wish.id !== wishId));
+    } catch (err) {
+      console.log('관심 목록 삭제에 실패했습니다.');
+    }
+  };
+
+  return (
+    <Wrapper>
+      <Container>
+        <Grid
+          columns={{
+            initial: 2,
+            md: 4,
+          }}
+          gap={16}
+        >
+          {wishes.map((wish: WishProduct) => (
+            <Container>
+              <DefaultGoodsItems
+                key={wish.id}
+                imageSrc={wish.option.product.imageUrl}
+                title={wish.option.product.name}
+                amount={wish.option.product.price}
+                subtitle={wish.option.name}
+              />
+              <Button mt={2} colorScheme="yellow" onClick={() => handleDeleteWish(wish.id)}>
+                삭제
+              </Button>
+            </Container>
+          ))}
+        </Grid>
+      </Container>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.section`
+  width: 100%;
+  padding: 28px 16px 180px;
+
+  @media screen and (min-width: ${breakpoints.sm}) {
+    padding: 40px 16px 360px;
+  }
+`;
