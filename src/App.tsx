0126f4d60442369c8b325537cfc4baf3ea5f6670
yunseong0404 <333665@naver.com>
2024-08-06 15:20:24 +0900

    fix: BrowserRouter 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { QueryClientProvider } from '@tanstack/react-query';
-import { BrowserRouter } from 'react-router-dom';
 
 import { queryClient } from './api/instance';
 import { AuthProvider } from './provider/Auth';
@@ -11,9 +10,7 @@ const App = () => {
     <ChakraProvider>
       <QueryClientProvider client={queryClient}>
         <AuthProvider>
-          <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Routes /> 
-          </BrowserRouter>
         </AuthProvider>
       </QueryClientProvider>
     </ChakraProvider>
